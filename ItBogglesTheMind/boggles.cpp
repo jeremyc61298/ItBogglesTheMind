4d6d05d11aa8769c1d7ebbfbe233de27a1355a41
Jeremy Campbell <jcampbell2@harding.edu>
2018-08-31 17:21:03 -0500

    Everything but the findWord function has been written.

--- ItBogglesTheMind/boggles.cpp
+++ ItBogglesTheMind/boggles.cpp
@@ -2,7 +2,7 @@
 // boggles.cpp
 // Jeremy Campbell
 // This program will determine if a word can be found within
-// random letter grid, only using letters once and never 
+// random 4x4 letter grid, only using letters once and never 
 // jumping further than one letter.
 // ----------------------------------------------------------
 
@@ -10,6 +10,7 @@
 #include <fstream>
 #include <vector>
 #include <string>
+#include <iomanip>
 
 using std::cout;
 using std::cin;
@@ -18,17 +19,43 @@ using std::ifstream;
 using std::ofstream;
 using std::vector;
 using std::string;
+using std::setw;
 
 struct LetterGrid
 {
-	public: 
-		void initialzeGrid(ifstream& in);
+	public:
+		LetterGrid() : sizeRowCol(4) {}
+		void initializeGrid(ifstream& in);
 		bool findWord(string word);
 
 	private:
 		vector<vector<char>> grid;
 		bool visited;
+		int sizeRowCol;
 };
+
+void LetterGrid::initializeGrid(ifstream& in)
+{
+	for (int i = 0; i < sizeRowCol; i++)
+	{
+		string line; 
+		vector<char> chars(4);
+		getline(in, line);
+		for (int j = 0; j < sizeRowCol; j++)
+		{
+			chars[j] = line[j];
+		}
+		grid.push_back(chars);
+	}
+}
+
+bool LetterGrid::findWord(string word)
+{
+	bool result = false;
+
+
+	return result;
+}
 
 int main()
 {
@@ -41,7 +68,16 @@ int main()
 
 	ofstream fout("boggle.out");
 	LetterGrid lg;
+	lg.initializeGrid(fin);
 
-
-
+	string word;
+	getline(fin, word);
+	while (word != "*")
+	{
+		bool result = lg.findWord(word);
+		fout << setw(17) << word;
+		(result) ? fout << "VALID" : fout << "NOT VALID";
+		fout << endl;
+		getline(fin, word);
+	}
 }
